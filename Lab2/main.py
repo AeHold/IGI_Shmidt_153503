46f9f35042980e499b43f64c817635d56bd521b5
Nikita Shmidt <shmidt0384@gmail.com>
2023-03-08 14:50:18 +0300

    Added counter for non-declarative sentences

--- Lab2/main.py
+++ Lab2/main.py
@@ -2,14 +2,19 @@ import re
 
 if __name__ == '__main__':
     str = input("Input text: ")
-    abbreviates = ('Dr', 'Mr')
+    abbreviations = ('Dr', 'Mr', 'Ms', 'Mrs', 'etc', 'e.g', 'B.C', 'A.C', 'inc')
     number_of_sentences = 0
+    nondeclarative_number = 0
     result = re.findall(r'((\w+)(\.\.\.|!|\?|\.)?)',str, flags=0)
 
     for word in result:
         if word[2] != '':
-            for trap in abbreviates:
-                if word[1] == trap:
+            for ban_words in abbreviations:
+                if word[1] == ban_words:
                     number_of_sentences -= 1
             number_of_sentences += 1
-    print(number_of_sentences)
+            if (word[2] == '?' or word[2] =='!'):
+                nondeclarative_number += 1
+    print('Number of sentences is:', number_of_sentences)
+    print('Number of non-declarative sentences is:', nondeclarative_number)
+  
