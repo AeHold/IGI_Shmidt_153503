fdf920833fb3d7fdda0dfe8d63a7090539571450
Nikita Shmidt <shmidt0384@gmail.com>
2023-03-09 12:26:02 +0300

    Updated Dockerfile and added average word counter

--- Lab2/main.py
+++ Lab2/main.py
@@ -5,6 +5,9 @@ if __name__ == '__main__':
     abbreviations = ('Dr', 'Mr', 'Ms', 'Mrs', 'etc', 'e.g', 'B.C', 'A.C', 'inc')
     number_of_sentences = 0
     nondeclarative_number = 0
+    average_length_sentences = []
+    counter = 0
+    length = 0
     result = re.findall(r'((\w+)(\.\.\.|!|\?|\.)?)',str, flags=0)
 
     for word in result:
@@ -15,6 +18,14 @@ if __name__ == '__main__':
             number_of_sentences += 1
             if (word[2] == '?' or word[2] =='!'):
                 nondeclarative_number += 1
+        if (word[2] != '.' or word[2] != '?' or word[2] != '!' or word[2] != '...'):
+            counter += 1
+            length += len(word[1])
+            print(word)
+        if (word[2] == '.' or word[2] == '?' or word[2] == '!' or word[2] == '...'):
+            average_length_sentences.append(length / counter)
+            length = 0
+            counter = 0
     print('Number of sentences is:', number_of_sentences)
     print('Number of non-declarative sentences is:', nondeclarative_number)
-  
+    print(average_length_sentences)
