f2679df55844b1e6715a955d68ae880bf5caa846
Nikita Shmidt <shmidt0384@gmail.com>
2023-03-22 11:43:09 +0300

    Added average word length counter

--- Lab2/main.py
+++ Lab2/main.py
@@ -6,8 +6,11 @@ if __name__ == '__main__':
     number_of_sentences = 0
     nondeclarative_number = 0
     average_length_sentences = []
+    average_word_length = 0
     counter = 0
     length = 0
+    word_counter = 0
+    word_length = 0
     result = re.findall(r'((\w+)(\.\.\.|!|\?|\.)?)',str, flags=0)
 
     for word in result:
@@ -21,11 +24,15 @@ if __name__ == '__main__':
         if (word[2] != '.' or word[2] != '?' or word[2] != '!' or word[2] != '...'):
             counter += 1
             length += len(word[1])
-            print(word)
         if (word[2] == '.' or word[2] == '?' or word[2] == '!' or word[2] == '...'):
             average_length_sentences.append(length / counter)
             length = 0
             counter = 0
+        if word != ' ':
+            word_counter += 1
+            word_length += len(word[1])
+            print(length)
     print('Number of sentences is:', number_of_sentences)
     print('Number of non-declarative sentences is:', nondeclarative_number)
     print(average_length_sentences)
+    print(word_length / word_counter)
