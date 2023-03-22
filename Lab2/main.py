40fb5b489cf9d33b6569571cb19a6cb962b3a743
Nikita Shmidt <shmidt0384@gmail.com>
2023-03-22 22:55:35 +0300

    Added top-K N-grams

--- Lab2/main.py
+++ Lab2/main.py
@@ -11,9 +11,14 @@ if __name__ == '__main__':
     length = 0
     word_counter = 0
     word_length = 0
+    n = int(input("Input N: "))
+    k = int(input("Input K: "))
+    n_grams = {}
     result = re.findall(r'((\w+)(\.\.\.|!|\?|\.)?)',str, flags=0)
-
+    reg = r'[0-9]+'
     for word in result:
+        if re.search(reg, word[1]):
+            continue
         if word[2] != '':
             for ban_words in abbreviations:
                 if word[1] == ban_words:
@@ -31,6 +36,11 @@ if __name__ == '__main__':
         if word != ' ':
             word_counter += 1
             word_length += len(word[1])
+        for i in range(len(word[1]) - n + 1):
+            s = word[0][i:i + n]
+            n_grams[s] = n_grams.get(s, 0) + 1
+    top_k = sorted(n_grams.items(), key = lambda x: x[1],reverse=True)[:k]
+    print(top_k)
     print('Number of sentences is:', number_of_sentences)
     print('Number of non-declarative sentences is:', nondeclarative_number)
     print('Average sentence length in characters is:', average_length_sentences)
