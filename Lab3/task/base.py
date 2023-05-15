91aee5e2498242534e9b686243372f9c7e1e393c
Nikita Shmidt <shmidt0384@gmail.com>
2023-05-15 19:27:15 +0300

    Added some constants and get_items function for FunctionType, CodeType, Collections, dict, and Builtin

--- Lab3/task/base.py
+++ Lab3/task/base.py
@@ -5,6 +5,7 @@ CellType, BuiltinMethodType, BuiltinFunctionType, WrapperDescriptorType,\
 MethodDescriptorType, MappingProxyType, GetSetDescriptorType, MemberDescriptorType
 from typing import Any, IO, Hashable, Collection, Iterable
 
+from utils.constants import TYPE_MAPPING
 
 class Serializer(ABC):
     _IGNORED_FIELDS: set[str] = (
@@ -28,3 +29,74 @@ class Serializer(ABC):
     def _type_from_str(cls, s: str, pattern: str) -> type:
         if not re.search(pattern, s):
             return NoneType
+        return TYPE_MAPPING[re.search(pattern, s).group(1)]
+    
+    def get_items(self, obj) -> dict[str, Any]:
+        if isinstance(obj, (BuiltinFunctionType, BuiltinMethodType)):
+            return {}
+        
+        if isinstance(obj, dict):
+            return obj
+        
+        elif isinstance(obj, Collection):
+            return dict(enumerate(obj))
+        
+        elif isinstance(obj, CodeType):
+            return {
+                "argcount": obj.co_argcount,
+                "posonlyargcount": obj.co_posonlyargcount,
+                "kwonlyargcount": obj.co_kwonlyargcount,
+                "nlocals": obj.co_nlocals,
+                "stacksize": obj.co_stacksize,
+                "flags": obj.co_flags,
+                "code": obj.co_code,
+                "consts": obj.co_consts,
+                "names": obj.co_names,
+                "varnames": obj.co_varnames,
+                "filename": obj.co_filename,
+                "name": obj.co_name,
+                "firstlineno": obj.co_firstlineno,
+                "lnotab": obj.co_lnotab,
+                "freevars": obj.co_freevars,
+                "cellvars": obj.co_cellvars,
+            }
+        
+        elif isinstance(obj, FunctionType):
+            if obj.__closure__ and "__class__" in obj.__code__.co_freevars:
+                closure = ([... for _ in obj.__closure__])if
+            elif obj.__closure__:
+                closure = ([cell.cell_contents for cell in obj.__closure__])
+            else:
+                closure = None
+            return {
+                "argcount": obj.__code__.co_argcount,
+                "posonlyargcount": obj.__code__.co_posonlyargcount,
+                "kwonlyargcount": obj.__code__.co_kwonlyargcount,
+                "nlocals": obj.__code__.co_nlocals,
+                "stacksize": obj.__code__.co_stacksize,
+                "flags": obj.__code__.co_flags,
+                "code": obj.__code__.co_code,
+                "consts": obj.__code__.co_consts,
+                "names": obj.__code__.co_names,
+                "varnames": obj.__code__.co_varnames,
+                "filename": obj.__code__.co_filename,
+                "name": obj.__code__.co_name,
+                "firstlineno": obj.__code__.co_firstlineno,
+                "lnotab": obj.__code__.co_lnotab,
+                "freevars": obj.__code__.co_freevars,
+                "cellvars": obj.__code__.co_cellvars,
+                "globals": {
+                    k: obj.__globals__[k]
+                    for k in (
+                        set(
+                            k for k, v in obj.__globals__.items()
+                            if isinstance(v, ModuleType)
+                        ) |
+                        set(obj.__globals__) &
+                        set(obj.__code__.co_names) -
+                        {obj.__name__}
+                    )
+                },
+                "closure": closure,
+                "qualname": obj.__qualname__
+            }
--- Lab3/task/utils/constants.py
+++ Lab3/task/utils/constants.py
@@ -6,3 +6,27 @@ from types import (
 PRIMITIVE_TYPES: tuple = (int, float, complex, str,
                            bool, NoneType, EllipsisType)
 
+TYPE_MAPPING = {
+    'int': int,
+    'float': float,
+    'complex': complex,
+    'str': str,
+    'bool': bool,
+    'NoneType': NoneType,
+    'ellipsis': EllipsisType,
+    'bytes': bytes,
+    'list': list,
+    'tuple': tuple,
+    'set': set,
+    'dict': dict,
+    'code': CodeType,
+    'cell': CellType,
+    'function': FunctionType,
+    'lambda': LambdaType,
+    'method': MethodType,
+    'staticmethod': staticmethod,
+    'classmethod': classmethod,
+    'type': type,
+    'module': ModuleType,
+    'object': object
+}
