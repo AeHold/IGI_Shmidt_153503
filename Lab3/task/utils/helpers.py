275d602e51219224fee130847be075160d26c158
Nikita Shmidt <shmidt0384@gmail.com>
2023-05-15 20:42:31 +0300

    Added output templates and formating helpers

---
+++ Lab3/task/utils/helpers.py
@@ -0,0 +1,33 @@
+from .constants import PRIMITIVE_TYPES
+from .templates import XML_ITEM
+
+class Formatter:
+    def move_line(string, indent):
+        return "\t" * indent + string
+    
+    def to_json(self, obj, dumps):
+        items_repr = ""
+
+        for k, v in obj.items():
+            if type(v) in PRIMITIVE_TYPES:
+                items_repr += f"\t{dumps(k)}: {dumps(v)}, \n"
+                continue
+
+            items_repr += f"\t{dumps(k)}: {{\n"
+            
+            for line in dumps(v).split("\n")[1:]:
+                items_repr += f"{self.move_line(line, 1)}\n"
+
+        return items_repr
+    
+    def to_xml(self, obj, dumps):
+        items_repr = ""
+
+        for k, v in obj.items():
+            items_repr += self.move_line(
+                XML_ITEM.format(
+                    key = dumps(k),
+                    value = dumps(v)
+                ), 1)
+            
+        return items_repr
