caf65288df9cc3e77dc3fd643bd3e3557852e87d
AeHold <73040884+AeHold@users.noreply.github.com>
2023-06-01 23:50:53 +0300

    Add files via upload

---
+++ vet_clinic/petclinic/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import generics, viewsets
+from .serializers import ClientSerializer
+from .models import Client
+
+
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
